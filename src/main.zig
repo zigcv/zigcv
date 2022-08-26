736fe62dc1352d129d9fdc8697b8a132f183343b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-08-26 17:48:23 +0100

    add svd

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,8 @@
 pub const c_api = @import("c_api.zig");
 pub usingnamespace @import("core.zig");
-pub usingnamespace @import("videoio.zig");
-pub usingnamespace @import("version.zig");
 pub usingnamespace @import("highgui.zig");
-pub usingnamespace @import("imgcodecs.zig");
 pub usingnamespace @import("objdetect.zig");
+pub usingnamespace @import("imgcodecs.zig");
+pub usingnamespace @import("svd.zig");
+pub usingnamespace @import("version.zig");
+pub usingnamespace @import("videoio.zig");
--- src/svd.zig
+++ src/svd.zig
@@ -1,2 +1,20 @@
+const c = @import("c_api.zig");
+const core = @import("core.zig");
+const Mat = core.Mat;
+
+pub fn svdCompute(
+    src: Mat,
+    w: *Mat,
+    u: *Mat,
+    vt: *Mat,
+) void {
+    c.SVD_Compute(
+        src.ptr,
+        w.*.ptr,
+        u.*.ptr,
+        vt.*.ptr,
+    );
+}
+
 //*    implementation done
-//     pub extern fn SVD_Compute(src: Mat, w: Mat, u: Mat, vt: Mat) void;
+//*    pub extern fn SVD_Compute(src: Mat, w: Mat, u: Mat, vt: Mat) void;
