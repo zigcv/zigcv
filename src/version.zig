30ee95aea7b4aa12168ff4f5565fe840dd99983a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-05 23:16:25 +0100

    fix test: use pkg-config

--- src/version.zig
+++ src/version.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const c = @import("c_api.zig");
-const core = @import("core.zig");
 
 pub fn openCVVersion() []const u8 {
     return std.mem.span(c.openCVVersion());
@@ -10,5 +9,21 @@ pub fn openCVVersion() []const u8 {
 //*    pub extern fn openCVVersion(...) [*c]const u8;
 
 test "show version" {
-    std.debug.print("version:\t{s}\n", .{openCVVersion()});
+    const testing = std.testing;
+    const allocator = testing.allocator;
+    const parseFloat = std.fmt.parseFloat;
+
+    const version = openCVVersion();
+    std.debug.print("\nversion:\t{s}\n", .{version});
+
+    const actual_version = (std.ChildProcess.exec(.{
+        .allocator = allocator,
+        .argv = &.{ "pkg-config", "--modversion", "opencv4" },
+    }) catch {
+        return;
+    }).stdout;
+
+    defer allocator.free(actual_version);
+
+    try testing.expectEqual(parseFloat(f32, actual_version), parseFloat(f32, version));
 }
