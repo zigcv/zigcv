d4fd85071783dd96321beff90fd4aa8d5d698353
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-08 12:44:28 +0100

    fic: STDVector and change comments for docs

--- src/imgcodecs.zig
+++ src/imgcodecs.zig
@@ -6,98 +6,99 @@ const utils = @import("utils.zig");
 const castToC = utils.castZigU8ToC;
 const Mat = core.Mat;
 
+/// IMReadFlag is one of the valid flags to use for the IMRead function.
 pub const IMReadFlag = enum(i9) {
-    // https://docs.opencv.org/4.6.0/d6/d87/imgcodecs_8hpp.html
-    // IMReadUnchanged return the loaded image as is (with alpha channel,
-    // otherwise it gets cropped).
+    /// https://docs.opencv.org/4.6.0/d6/d87/imgcodecs_8hpp.html
+    /// IMReadUnchanged return the loaded image as is (with alpha channel,
+    /// otherwise it gets cropped).
     unchanged = -1,
 
-    // IMReadGrayScale always convert image to the single channel
-    // grayscale image.
+    /// IMReadGrayScale always convert image to the single channel
+    /// grayscale image.
     gray_scale = 0,
 
-    // IMReadColor always converts image to the 3 channel BGR color image.
+    /// IMReadColor always converts image to the 3 channel BGR color image.
     color = 1,
 
-    // IMReadAnyDepth returns 16-bit/32-bit image when the input has the corresponding
-    // depth, otherwise convert it to 8-bit.
+    /// IMReadAnyDepth returns 16-bit/32-bit image when the input has the corresponding
+    /// depth, otherwise convert it to 8-bit.
     any_depth = 2,
 
-    // IMReadAnyColor the image is read in any possible color format.
+    /// IMReadAnyColor the image is read in any possible color format.
     any_color = 4,
 
-    // IMReadLoadGDAL uses the gdal driver for loading the image.
+    /// IMReadLoadGDAL uses the gdal driver for loading the image.
     load_GDAL = 8,
 
-    // IMReadReducedGrayscale2 always converts image to the single channel grayscale image
-    // and the image size reduced 1/2.
+    /// IMReadReducedGrayscale2 always converts image to the single channel grayscale image
+    /// and the image size reduced 1/2.
     reduced_grayscale2 = 16,
 
-    // IMReadReducedColor2 always converts image to the 3 channel BGR color image and the
-    // image size reduced 1/2.
+    /// IMReadReducedColor2 always converts image to the 3 channel BGR color image and the
+    /// image size reduced 1/2.
     reduced_color2 = 17,
 
-    // IMReadReducedGrayscale4 always converts image to the single channel grayscale image and
-    // the image size reduced 1/4.
+    /// IMReadReducedGrayscale4 always converts image to the single channel grayscale image and
+    /// the image size reduced 1/4.
     reduced_grayscale4 = 32,
 
-    // IMReadReducedColor4 always converts image to the 3 channel BGR color image and
-    // the image size reduced 1/4.
+    /// IMReadReducedColor4 always converts image to the 3 channel BGR color image and
+    /// the image size reduced 1/4.
     reduced_color4 = 33,
 
-    // IMReadReducedGrayscale8 always convert image to the single channel grayscale image and
-    // the image size reduced 1/8.
+    /// IMReadReducedGrayscale8 always convert image to the single channel grayscale image and
+    /// the image size reduced 1/8.
     reduced_grayscale8 = 64,
 
-    // IMReadReducedColor8 always convert image to the 3 channel BGR color image and the
-    // image size reduced 1/8.
+    /// IMReadReducedColor8 always convert image to the 3 channel BGR color image and the
+    /// image size reduced 1/8.
     reduced_color8 = 65,
 
-    // IMReadIgnoreOrientation do not rotate the image according to EXIF's orientation flag.
+    /// IMReadIgnoreOrientation do not rotate the image according to EXIF's orientation flag.
     ignore_orientation = 128,
 };
 
 pub const IMWriteFlag = enum(u9) {
-    //IMWriteJpegQuality is the quality from 0 to 100 for JPEG (the higher is the better). Default value is 95.
+    ///IMWriteJpegQuality is the quality from 0 to 100 for JPEG (the higher is the better). Default value is 95.
     jpeg_quality = 1,
 
-    // IMWriteJpegProgressive enables JPEG progressive feature, 0 or 1, default is False.
+    /// IMWriteJpegProgressive enables JPEG progressive feature, 0 or 1, default is False.
     jpeg_progressive = 2,
 
-    // IMWriteJpegOptimize enables JPEG optimization, 0 or 1, default is False.
+    /// IMWriteJpegOptimize enables JPEG optimization, 0 or 1, default is False.
     jpeg_optimize = 3,
 
-    // IMWriteJpegRstInterval is the JPEG restart interval, 0 - 65535, default is 0 - no restart.
+    /// IMWriteJpegRstInterval is the JPEG restart interval, 0 - 65535, default is 0 - no restart.
     jpeg_rst_interval = 4,
 
-    // IMWriteJpegLumaQuality separates luma quality level, 0 - 100, default is 0 - don't use.
+    /// IMWriteJpegLumaQuality separates luma quality level, 0 - 100, default is 0 - don't use.
     jpeg_luma_quality = 5,
 
-    // IMWriteJpegChromaQuality separates chroma quality level, 0 - 100, default is 0 - don't use.
+    /// IMWriteJpegChromaQuality separates chroma quality level, 0 - 100, default is 0 - don't use.
     jpeg_chroma_quality = 6,
 
-    // IMWritePngCompression is the compression level from 0 to 9 for PNG. A
-    // higher value means a smaller size and longer compression time.
-    // If specified, strategy is changed to IMWRITE_PNG_STRATEGY_DEFAULT (Z_DEFAULT_STRATEGY).
-    // Default value is 1 (best speed setting).
+    /// IMWritePngCompression is the compression level from 0 to 9 for PNG. A
+    /// higher value means a smaller size and longer compression time.
+    /// If specified, strategy is changed to IMWRITE_PNG_STRATEGY_DEFAULT (Z_DEFAULT_STRATEGY).
+    /// Default value is 1 (best speed setting).
     png_compression = 16,
 
-    // IMWritePngStrategy is one of cv::IMWritePNGFlags, default is IMWRITE_PNG_STRATEGY_RLE.
+    /// IMWritePngStrategy is one of cv::IMWritePNGFlags, default is IMWRITE_PNG_STRATEGY_RLE.
     png_strategy = 17,
 
-    // IMWritePngBilevel is the binary level PNG, 0 or 1, default is 0.
+    /// IMWritePngBilevel is the binary level PNG, 0 or 1, default is 0.
     png_bilevel = 18,
 
-    // IMWritePxmBinary for PPM, PGM, or PBM can be a binary format flag, 0 or 1. Default value is 1.
+    /// IMWritePxmBinary for PPM, PGM, or PBM can be a binary format flag, 0 or 1. Default value is 1.
     pxm_binary = 32,
 
-    // IMWriteWebpQuality is the quality from 1 to 100 for WEBP (the higher is
-    // the better). By default (without any parameter) and for quality above
-    // 100 the lossless compression is used.
+    /// IMWriteWebpQuality is the quality from 1 to 100 for WEBP (the higher is
+    /// the better). By default (without any parameter) and for quality above
+    /// 100 the lossless compression is used.
     webp_quality = 64,
 
-    // IMWritePamTupletype sets the TUPLETYPE field to the corresponding string
-    // value that is defined for the format.
+    /// IMWritePamTupletype sets the TUPLETYPE field to the corresponding string
+    /// value that is defined for the format.
     pam_tupletype = 128,
 
     tiff_resunit = 256,
@@ -125,26 +126,32 @@ pub const FileExt = enum(u2) {
     }
 };
 
-const IMWriteParam = struct { f: IMWriteFlag, v: i32 };
+pub const IMWriteParam = struct { f: IMWriteFlag, v: i32 };
 
-// IMRead reads an image from a file into a Mat.
-// The flags param is one of the IMReadFlag flags.
-// If the image cannot be read (because of missing file, improper permissions,
-// unsupported or invalid format), the function returns an empty Mat.
-//
-// For further details, please see:
-// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
-//
+const STDVector = packed struct {
+    value: usize,
+    before: usize,
+    after: usize,
+};
+
+/// IMRead reads an image from a file into a Mat.
+/// The flags param is one of the IMReadFlag flags.
+/// If the image cannot be read (because of missing file, improper permissions,
+/// unsupported or invalid format), the function returns an empty Mat.
+///
+/// For further details, please see:
+/// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
+///
 pub fn imRead(filename: []const u8, flags: IMReadFlag) !Mat {
     var cMat: c.Mat = c.Image_IMRead(castToC(filename), @enumToInt(flags));
     return try Mat.fromC(cMat);
 }
 
-// IMWrite writes a Mat to an image file.
-//
-// For further details, please see:
-// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gabbc7ef1aa2edfaa87772f1202d67e0ce
-//
+/// IMWrite writes a Mat to an image file.
+///
+/// For further details, please see:
+/// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gabbc7ef1aa2edfaa87772f1202d67e0ce
+///
 pub fn imWrite(filename: []const u8, img: Mat) !void {
     const result = c.Image_IMWrite(castToC(filename), img.ptr);
     if (!result) {
@@ -152,13 +159,13 @@ pub fn imWrite(filename: []const u8, img: Mat) !void {
     }
 }
 
-// IMWriteWithParams writes a Mat to an image file. With that func you can
-// pass compression parameters.
-//
-// For further details, please see:
-// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gabbc7ef1aa2edfaa87772f1202d67e0ce
-// https://docs.opencv.org/4.6.0/d8/d6a/group__imgcodecs__flags.html
-//
+/// IMWriteWithParams writes a Mat to an image file. With that func you can
+/// pass compression parameters.
+///
+/// For further details, please see:
+/// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#gabbc7ef1aa2edfaa87772f1202d67e0ce
+/// https://docs.opencv.org/4.6.0/d8/d6a/group__imgcodecs__flags.html
+///
 pub fn imWriteWithParams(filename: []const u8, img: Mat, comptime params: []const IMWriteParam) !void {
     const c_params = comptime blk: {
         const len = params.len * 2;
@@ -178,14 +185,14 @@ pub fn imWriteWithParams(filename: []const u8, img: Mat, comptime params: []cons
     }
 }
 
-// IMDecode reads an image from a buffer in memory.
-// The function IMDecode reads an image from the specified buffer in memory.
-// If the buffer is too short or contains invalid data, the function
-// returns an empty matrix.
-//
-// For further details, please see:
-// https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga26a67788faa58ade337f8d28ba0eb19e
-//
+/// IMDecode reads an image from a buffer in memory.
+/// The function IMDecode reads an image from the specified buffer in memory.
+/// If the buffer is too short or contains invalid data, the function
+/// returns an empty matrix.
+///
+/// For further details, please see:
+/// https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga26a67788faa58ade337f8d28ba0eb19e
+///
 pub fn imDecode(buf: []const u8, flags: IMReadFlag) !Mat {
     if (buf.len == 0) {
         return Mat.init();
@@ -197,16 +204,16 @@ pub fn imDecode(buf: []const u8, flags: IMReadFlag) !Mat {
     return try Mat.fromC(c.Image_IMDecode(data, @enumToInt(flags)));
 }
 
-// IMEncode encodes an image Mat into a memory buffer.
-// This function compresses the image and stores it in the returned memory buffer,
-// using the image format passed in in the form of a file extension string.
-//
-// For further details, please see:
-// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga461f9ac09887e47797a54567df3b8b63
-//
+/// IMEncode encodes an image Mat into a memory buffer.
+/// This function compresses the image and stores it in the returned memory buffer,
+/// using the image format passed in in the form of a file extension string.
+///
+/// For further details, please see:
+/// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga461f9ac09887e47797a54567df3b8b63
+///
 pub fn imEncode(allocator: std.mem.Allocator, file_ext: FileExt, img: Mat) !std.ArrayList(u8) {
-    var c_vector: struct { std_vector_opaq: [3]usize } = undefined;
-    var cvp = &(c_vector.std_vector_opaq[0]);
+    var c_vector: STDVector = undefined;
+    var cvp = &c_vector;
     c.StdByteVectorInitialize(cvp);
     defer c.StdByteVectorFree(cvp);
     c.Image_IMEncode(castToC(file_ext.toString()), img.ptr, cvp);
@@ -222,14 +229,13 @@ pub fn imEncode(allocator: std.mem.Allocator, file_ext: FileExt, img: Mat) !std.
     return buf;
 }
 
-// IMEncodeWithParams encodes an image Mat into a memory buffer.
-// This function compresses the image and stores it in the returned memory buffer,
-// using the image format passed in in the form of a file extension string.
-//
-// For further details, please see:
-// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga461f9ac09887e47797a54567df3b8b63
-//
-
+/// IMEncodeWithParams encodes an image Mat into a memory buffer.
+/// This function compresses the image and stores it in the returned memory buffer,
+/// using the image format passed in in the form of a file extension string.
+///
+/// For further details, please see:
+/// http://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga461f9ac09887e47797a54567df3b8b63
+///
 pub fn imEncodeWithParams(allocator: std.mem.Allocator, file_ext: FileExt, img: Mat, comptime params: []const IMWriteParam) !std.ArrayList(u8) {
     // TODO: Failed on M1 Mac
     if (builtin.os.tag == .macos and builtin.target.cpu.arch == .aarch64) @compileError("imEncodeWithParams is not supported on M1 Mac");
@@ -245,8 +251,8 @@ pub fn imEncodeWithParams(allocator: std.mem.Allocator, file_ext: FileExt, img: 
             .length = len,
         };
     };
-    var c_vector: struct { std_vector_opaq: [3]usize } = undefined;
-    var cvp = &(c_vector.std_vector_opaq[0]);
+    var c_vector: STDVector = undefined;
+    var cvp = &c_vector;
     c.StdByteVectorInitialize(cvp);
     defer c.StdByteVectorFree(cvp);
     c.Image_IMEncode_WithParams(castToC(file_ext.toString()), img.ptr, c_params, cvp);
