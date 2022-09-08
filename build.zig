b443d85eb3813cd78a2f40933781f4098f3ec6e3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-08 12:45:16 +0100

    add docs

--- build.zig
+++ build.zig
@@ -70,6 +70,13 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
+
+    const docs = b.addTest("src/main.zig");
+    zigcv.link(docs);
+    zigcv.addAsPackage(docs);
+    docs.emit_docs = .emit;
+    const docs_step = b.step("docs", "Generate docs");
+    docs_step.dependOn(&docs.step);
 }
 
 inline fn thisDir() []const u8 {
