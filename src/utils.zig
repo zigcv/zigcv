3889d92b60c5322005d599cbec3d935b3566e29a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-20 21:29:56 +0100

    fix wrap function

--- src/utils.zig
+++ src/utils.zig
@@ -17,13 +17,50 @@ pub fn fromCStructsToArrayList(from_array: anytype, from_array_length: i32, comp
     return arr;
 }
 
-fn wrap(comptime T: type, value: T) !T {
+fn wrap(comptime T: type, value: anytype) !T {
     return value;
 }
 
 pub fn ensurePtrNotNull(ptr: ?*anyopaque) !*anyopaque {
     if (ptr == null) return error.AllocationError;
     return ptr.?;
+}
+
+pub fn downloadFile(url: []const u8, dir: []const u8, allocator: std.mem.Allocator) !void {
+    if (dir[dir.len - 1] != '/') unreachable;
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    const arena_allocator = arena.allocator();
+
+    var split_url = std.mem.split(u8, url, "/");
+    var filename: []const u8 = undefined;
+    while (split_url.next()) |s| {
+        filename = s;
+    }
+    const dir_filename = try std.fmt.allocPrint(
+        arena_allocator,
+        "{s}{s}",
+        .{ dir, filename },
+    );
+    _ = std.fs.cwd().statFile(dir_filename) catch |err| {
+        std.debug.print("filename: {s},\terror: {any}\n", .{ dir_filename, err });
+        if (err != error.FileNotFound) unreachable;
+        var child = std.ChildProcess.init(
+            &.{
+                "curl",
+                "--create-dirs",
+                "-O",
+                "--output-dir",
+                dir,
+                url,
+            },
+            arena_allocator,
+        );
+        child.stderr = std.io.getStdErr();
+        child.stdout = std.io.getStdOut();
+        _ = child.spawnAndWait() catch unreachable;
+    };
 }
 
 test "ensureNotNull" {
