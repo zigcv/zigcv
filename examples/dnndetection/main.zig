a055293c41cfe3cb3d39a5c27f9e3752551265a1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-23 17:47:56 +0100

    download model

--- examples/dnndetection/main.zig
+++ examples/dnndetection/main.zig
@@ -10,15 +10,25 @@
 // You will also need the prototxt config file:
 // https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
 //
+// To perform object tracking with the Tensorflow model:
+//
+// Download and extract the model file named "frozen_inference_graph.pb" from:
+// http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz
+//
+// You will also need the pbtxt config file:
+// https://gist.githubusercontent.com/dkurt/45118a9c57c38677b65d6953ae62924a/raw/b0edd9e8c992c25fe1c804e77b06d20a89064871/ssd_mobilenet_v1_coco_2017_11_17.pbtxt
+//
 
 const std = @import("std");
 const cv = @import("zigcv");
 const downloadFile = cv.utils.downloadFile;
 const Mat = cv.Mat;
+const Size = cv.Size;
+const c_api = cv.c_api;
 
 const cache_dir = "./zig-cache/tmp/";
 const model_path = cache_dir ++ "res10_300x300_ssd_iter_140000.caffemodel";
-const mdoel_url = "https://github.com/opencv/opencv_3rdparty/raw/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel";
+const model_url = "https://github.com/opencv/opencv_3rdparty/raw/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel";
 const config_path = cache_dir ++ "deploy.prototxt";
 const config_url = "https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt";
 
@@ -49,7 +59,7 @@ pub fn main() anyerror!void {
     defer img.deinit();
 
     // open DNN object tracking model
-    try downloadFile(mdoel_url, cache_dir, allocator);
+    try downloadFile(model_url, cache_dir, allocator);
     try downloadFile(config_url, cache_dir, allocator);
     var net = cv.Net.readNet(model_path, config_path) catch |err| {
         std.debug.print("Error: {}\n", .{err});
@@ -78,7 +88,14 @@ pub fn main() anyerror!void {
             continue;
         }
 
-        var blob = try cv.Blob.initFromImage(img, ratio, cv.Size{ .width = 300, .height = 300 }, mean, swap_rgb, false);
+        var blob = try cv.Blob.initFromImage(
+            img,
+            ratio,
+            Size.init(300, 300),
+            mean,
+            swap_rgb,
+            false,
+        );
         defer blob.deinit();
 
         net.setInput(blob, "");
@@ -95,13 +112,18 @@ pub fn main() anyerror!void {
     }
 }
 
+// performDetection analyzes the results from the detector network,
+// which produces an output blob with a shape 1x1xNx7
+// where N is the number of detections, and each detection
+// is a vector of float values
+// [batchId, classId, confidence, left, top, right, bottom]
 fn performDetection(frame: *Mat, results: Mat) void {
     const green = cv.Color{ .g = 255 };
     var i: usize = 0;
     while (i < results.total()) {
         var confidence = results.get(f32, 0, i + 2);
-        const cols = @intToFloat(f32, frame.*.cols());
-        const rows = @intToFloat(f32, frame.*.rows());
+        const cols = @intToFloat(f32, frame.cols());
+        const rows = @intToFloat(f32, frame.rows());
         if (confidence > 0.5) {
             var left = @floatToInt(i32, results.get(f32, 0, i + 3) * cols);
             var top = @floatToInt(i32, results.get(f32, 0, i + 4) * rows);
